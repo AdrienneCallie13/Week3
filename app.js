0104d485554c27a97fe7ad2af8f37cdb587123ab
Adrienne <87454125+AdrienneCallie13@users.noreply.github.com>
2021-09-07 22:34:23 +1000

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+/* String Methods Examples */
+
+var stringOne = "SIT120 is the best unit to learn"
+var stringTwo = "frontend and backend development" 
+
+// toLowerCase()
+console.log(stringOne.toLowerCase())
+
+// toUpperCase()
+console.log(stringOne.toUpperCase())
+
+// charAt()
+console.log(stringOne.charAt(1))
+
+// includes()
+console.log(stringTwo.includes("learn"))
